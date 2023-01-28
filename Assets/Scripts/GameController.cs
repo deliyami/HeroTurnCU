0d4d1ddf3767cdb07ec21253d88ef5147ccaad55
deliyami <dkdidudtn@gmail.com>
2023-01-28 17:44:51 +0900

    UPDATE - Map loadding mechanism

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -10,6 +10,8 @@ public class GameController : MonoBehaviour
     [SerializeField] Camera worldCamera;
     GameState state;
     GameState stateBeforePuase;
+    public SceneDetails CurrentScene { get; private set; }
+    public SceneDetails PrevScene { get; private set; }
     public static GameController Instance { get; private set; }
 
     private void Awake()
@@ -106,5 +108,11 @@ public class GameController : MonoBehaviour
         {
             DialogManager.Instance.HandleUpdate();
         }
+    }
+
+    public void SetCurrentScene(SceneDetails currScene)
+    {
+        PrevScene = CurrentScene;
+        CurrentScene = currScene;
     }
 }
