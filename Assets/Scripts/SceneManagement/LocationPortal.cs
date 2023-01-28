8d4de7b279f9958491f596f7968516a24292b3ed
deliyami <dkdidudtn@gmail.com>
2023-01-28 18:15:13 +0900

    UPDATE - location portal
    not use scene teleport

---
+++ Assets/Scripts/SceneManagement/LocationPortal.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+// 화면 전환 없이 포탈 사용
+public class LocationPortal : MonoBehaviour, IPlayerTriggerable
+{
+
+    [SerializeField] DestinationIdentifier destinationPortal;
+    [SerializeField] Transform spawnPoint;
+
+    PlayerController player;
+    public void OnPlayerTriggered(PlayerController player)
+    {
+        this.player = player;
+        StartCoroutine(Teleport());
+    }
+    Fader fader;
+    private void Start()
+    {
+        fader = FindObjectOfType<Fader>();
+    }
+
+    IEnumerator Teleport()
+    {
+        GameController.Instance.PauseGame(true);
+        yield return fader.FadeIn(0.5f);
+
+        // Debug.Log("포탈 사용");
+
+        var destPortal = FindObjectsOfType<LocationPortal>().First(x => x!= this && x.destinationPortal == this.destinationPortal);
+        player.transform.position = destPortal.SpawnPoint.position;
+        player.Character.SetPositionAndSnapToTile(destPortal.SpawnPoint.position);
+        
+        yield return fader.FadeOut(0.5f);
+        GameController.Instance.PauseGame(false);
+    }
+
+    public Transform SpawnPoint => spawnPoint;
+}
